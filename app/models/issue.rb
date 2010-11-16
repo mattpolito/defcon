09af0428293ae65b0fab28b70999fc01f185fba8
Matt Polito <matt.polito@gmail.com>
2010-11-16 07:20:29 -0600

    validations on status and priority added default value and changed data type of priority_number

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,10 +4,11 @@ class Issue
   field :description
   field :author
   field :status, :default => "unassigned"
-  field :priority_number
+  field :priority_number, :type => Integer, :default => 0
 
+  # Validations
   validates :title, :presence => true
   validates :description, :presence => true
-
-
+  validates :status, :presence => true
+  validates :priority_number, :presence => true
 end
--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe Issue do
   it { should have_field(:title).of_type(String) }
   it { should have_field(:description).of_type(String) }
+  it { should have_field(:author).of_type(String) }
+  it { should have_field(:status).of_type(String) }
+  it { should have_field(:priority_number).of_type(Integer) }
   
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:status) }
+  it { should validate_presence_of(:priority_number) }
 
   let(:valid_attributes) do
     {
@@ -31,5 +36,13 @@ describe Issue do
 
     it { should have(1).error_on(:title) }
     it { should have(1).error_on(:description) }
+
+    it "has a default status of 'unassigned'" do
+      subject.status.should == 'unassigned' 
+    end
+
+    it "has a default priority of '0'" do
+      subject.priority_number.should == 0
+    end
   end
 end
