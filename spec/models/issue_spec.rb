56d5c59a7ed7817ce84c3494b7291052d7dc158a
Matt Polito <matt.polito@gmail.com>
2010-11-15 18:11:03 -0600

    added some sugary validation checks

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe Issue do
   it { should have_field(:title).of_type(String) }
   it { should have_field(:description).of_type(String) }
+  
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 
   let(:valid_attributes) do
     {
