e0e6966558ca89d8bd09f485e683f34b227179c9
Matt Polito <matt.polito@gmail.com>
2011-07-06 18:01:31 -0500

    rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+# If you have a Gemfile, require the default gems, the ones in the
+# current environment and also include :assets gems if in development
+# or test environments.
+Bundler.require *Rails.groups(:assets => %w(development test)) if defined?(Bundler)
 
 module Defcon
   class Application < Rails::Application
@@ -30,25 +31,15 @@ module Defcon
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Please note that JavaScript expansions are *ignored altogether* if the asset
-    # pipeline is enabled (see config.assets.enabled below). Put your defaults in
-    # app/assets/javascripts/application.js in that case.
-    #
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w()
-
-    config.generators.test_framework = false
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    # Enable IdentityMap for Active Record, to disable set to false or remove the line below.
-    config.active_record.identity_map = true
-
+    
     # Enable the asset pipeline
     config.assets.enabled = true
+    
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Defcon::Application.config.secret_token = '52a0d0954fdcf3d46211211665526e308f11e1bbbbbbfbb8e2cb8f2a457c1a3817dda6a42f5e08f51a87616e4bbe03bd4e7b220cc907d2c81101559fb177afe2'
+Defcon::Application.config.secret_token = 'a12cfc9c9557bf3138d403173fca600663f017f4515c925cfbc36a5cbff43e6ea6d040aa1c2a1f5bf97351c550eb3f09a5058ac355e2cc8d67fc01ba51c2864a'
