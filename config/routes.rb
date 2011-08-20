21e9ecf6472a4ecb64784f6af56957ddad1eaf29
Matt Polito <matt.polito@gmail.com>
2011-08-19 21:51:45 -0400

    Removing subdomain constraints for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Defcon::Application.routes.draw do
-  constraints(Subdomain) do
-    devise_for :users do
-      get "sign_in", :to => "devise/sessions#new"
-      get "sign_out", :to => "devise/sessions#destroy"
+  resources :issues do
+    collection do
+      post 'prioritize'
+      post 'assignment'
     end
-
-    resources :issues do
-      collection do
-        post 'prioritize'
-        post 'assignment'
-      end
-    end
-
-    root :to => "issues#index"
   end
+
+  root :to => "issues#index"
 end
